798fe9f3ba837d76e8a5ec111d5da6e82b642f34
m.tomashivskyi <tomashivskymykola@gmail.com>
2025-03-24 22:57:13 +0200

    added generic repository

---
+++ URLShortenerBackend/Repositories/IGeneralRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace URLShortenerBackend.Repositories
+{
+    public interface IGeneralRepository<T> where T : class
+    {
+        public Task<bool> AddAsync(T entity);
+        public Task<bool> DeleteAsync(T entity);
+        public Task UpdateAsync(T entity);
+        public Task<List<T>> GetAllAsync();
+        public Task<T?> GetByIdAsync(int id);
+        public Task<IEnumerable<T>> FindByConditionAsync(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ URLShortenerBackend/Repositories/Implementations/GeneralRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using URLShortenerBackend.Data;
+
+namespace URLShortenerBackend.Repositories.Implementations
+{
+    public class GeneralRepository<T> : IGeneralRepository<T> where T : class
+    {
+        private readonly DbContextEF _context;
+
+        // Конструктор для DI
+        public GeneralRepository(DbContextEF context)
+        {
+            _context = context;
+        }
+
+        public DbSet<T> Entity => _context.Set<T>();
+
+        public async Task<bool> AddAsync(T entity)
+        {
+            await Entity.AddAsync(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteAsync(T entity)
+        {
+            Entity.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await Entity.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _context.FindAsync<T>(id);
+        }
+
+        public async Task<IEnumerable<T>> FindByConditionAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await Entity.Where(predicate).ToListAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
